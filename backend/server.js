476406bfd459d14da456262ab775abb73c43ff60
Samuel Cohen <samco0807@gmail.com>
2025-01-29 17:57:43 +0200

    link front end to back end

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,27 @@
 import express from "express";
 import cors from "cors";
 import router from "./src/routes/eventRoutes.js";
-const app = express();
 const port = 3000;
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
+const app = express();
 
+// Resolve __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve React static files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  });
+
+// Middleware
 app.use(cors());
 app.use(express.json());
+
 app.use(router);
 
 app.get("/", (req, res) => res.send("Hello World!"));
