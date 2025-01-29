30f43cfb2ba54222ff6970fadb919342daf319a2
Samuel Cohen <samco0807@gmail.com>
2025-01-29 19:09:36 +0200

    last change

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,8 @@ import cors from "cors";
 import router from "./src/routes/eventRoutes.js";
 const port = 3000;
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 dotenv.config();
 const app = express();
-
-// Resolve __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve React static files
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-  });
 
 // Middleware
 app.use(cors());
