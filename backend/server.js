cb5022b443e72165471ee488d08a6960a31688ed
Samuel Cohen <samco0807@gmail.com>
2025-01-31 11:01:48 +0200

    definitive commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import router from "./src/routes/eventRoutes.js";
-const port = process.env.PORT || 3000;
+const port = 3000;
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
@@ -10,21 +10,21 @@ dotenv.config();
 const app = express();
 
 // Resolve __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Serve React static files
-if (process.env.PROD === "yes") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.PROD === "yes") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+//   });
+// }
 
 app.use(router);
 
