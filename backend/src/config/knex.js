b5de3a6f1b6335668016d69c787ab88a17039b08
samco0807 <samco0807@gmail.com>
2025-07-15 12:03:47 +0300

    new save

--- backend/src/config/knex.js
+++ backend/src/config/knex.js
@@ -1,20 +1,24 @@
 // backend/src/db/knex.js
 import knex from "knex";
 import dotenv from "dotenv";
+// import { knexSnakeCaseMappers } from "objection"; // to install to convert snake_case into camelCase
 dotenv.config();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
-const db = knex({
-  client: "pg",
-  connection: {
-    host: PGHOST,
-    user: PGUSER,
-    database: PGDATABASE,
-    password: PGPASSWORD,
-    ssl: { rejectUnauthorized: false },
+const db = knex(
+  {
+    client: "pg",
+    connection: {
+      host: PGHOST,
+      user: PGUSER,
+      database: PGDATABASE,
+      password: PGPASSWORD,
+      ssl: { rejectUnauthorized: false },
+    },
   },
-});
+  // ...knexSnakeCaseMappers()
+);
 
 export default db;
 
